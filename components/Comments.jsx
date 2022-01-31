500a899480af5e35d6e5d0186081b0bc13d51420
Junyu Zhu <alexzhuisme@gmail.com>
2022-01-31 04:30:40 -0800

    Comments updated

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 const Comments = () => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+  
   return (
-    <div>Comments</div>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments</h3>
+    </div>
     );
 };
 
